c2a00495b6cf3e8cf1e97f016b5a56fb2daac525
jpondefy <146007223+jpondefy@users.noreply.github.com>
2024-01-18 17:11:58 -0300

    correction of find user service

--- src/main/java/br/com/picpaychallenge/application/core/service/FindUserService.java
+++ src/main/java/br/com/picpaychallenge/application/core/service/FindUserService.java
@@ -2,14 +2,15 @@ package br.com.picpaychallenge.application.core.service;
 
 import br.com.picpaychallenge.application.core.domain.User;
 import br.com.picpaychallenge.application.ports.outbound.CreateUserPort;
+import br.com.picpaychallenge.application.ports.outbound.FindUserPort;
 
 import java.util.List;
 
 public class FindUserService {
 
-    private final CreateUserPort repository;
+    private final FindUserPort repository;
 
-    public FindUserService(CreateUserPort repository) {
+    public FindUserService(FindUserPort repository) {
         this.repository = repository;
     }
 
